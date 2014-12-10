6ec866399198f05940f8568571213a525748d055
William Dembinski <wdembiz@gmail.com>
2014-12-10 10:46:10 -0800

    Fixed the original spree gem, moved the changes into the actual project.

---
+++ app/overrides/testoverride.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path  => "spree/shared/header",
+                     :insert_after => "figure#logo",
+                     :text          => "<p>Regis================================================tration is the future!</p>",
+                     :name          => "testoverride")
